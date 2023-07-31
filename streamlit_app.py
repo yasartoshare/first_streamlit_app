8c4f2d22a6f537e484d238ddded3f758bbc56d56
yasartoshare <66778161+yasartoshare@users.noreply.github.com>
2023-07-31 18:33:36 +0530

    Update streamlit_app.py
    Added Personalized Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Spinach & Smoothie')
 streamlit.text('🐔 Boiled Egg')
-streamlit.text('🥑 Avodato Toast')
+streamlit.text('🥑🍞 Avodato Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

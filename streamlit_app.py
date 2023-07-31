3d0a84e95735c5c2e7564638ad0211e054c57eaf
yasartoshare <66778161+yasartoshare@users.noreply.github.com>
2023-07-31 18:29:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('New Streamlit App')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Spinach & Smoothie')
+streamlit.text('Boiled Egg')

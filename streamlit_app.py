45377a41b8512752cf29fcb1e97fbdda27f97cfc
yasartoshare <66778161+yasartoshare@users.noreply.github.com>
2023-07-31 18:32:27 +0530

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('New Streamlit App')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Spinach & Smoothie')
-streamlit.text('Boiled Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Spinach & Smoothie')
+streamlit.text('🐔 Boiled Egg')
+streamlit.text('🥑 Avodato Toast')
